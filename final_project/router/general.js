44874946400c63290a9982896da044b32ce8e262
Nicholas-09785 <npetrelli@live.ca>
2026-07-29 19:15:46 -0400

    finished general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,32 +13,55 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  const obj = req.query.books;
-  return res.status(300).json({message: JSON.stringify(obj)});
+  return res.status(300).json({message: JSON.stringify(books)});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  for (const key in books) {
+    const num = books[key];
+    if (Math.floor(isbn) == num)
+      return res.status(300).json({message: books[key].title});
+  }
+  return res.status(300).json({message: JSON.stringify(isbn)});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  for (const key in books) {
+    const authorPerson = books[key].author;
+    if (authorPerson == author)
+      return res.status(300).json({message: books[key].title});
+  }
+  return res.status(404).json({message: "Book not found based off author"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  for (const key in books) {
+    const bookTitle = books[key].title;
+    if (bookTitle == title)
+      return res.status(300).json({message: books[key].author});
+  }
+  return res.status(404).json({message: "Book not found based off title"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  for (const key in books) {
+    const num = books[key];
+    if (Math.floor(isbn) == num)
+      return res.status(300).json({message: books[key].review});
+  }
+  return res.status(300).json({message: JSON.stringify(review)});
 });
 
 module.exports.general = public_users;
